7da99f741d59b11c6eecb746cc31aa534643cea7
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2024-07-09 12:04:07 +0200

    init repo

---
+++ tests/test_ned_template.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""Tests for `ned_template` package."""
+
+import pytest
+
+
+from ned_template import ned_template
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
